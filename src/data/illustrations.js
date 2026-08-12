f298e9e38600bdcd138fbc6f10d309993df776c5
issyj1 <issyjay@hotmail.co.uk>
2026-08-12 16:01:06 +0200

    Update contact link

--- src/data/illustrations.js
+++ src/data/illustrations.js
@@ -11,8 +11,5 @@ export const illustrationImages = [
       src: `${import.meta.env.BASE_URL}img/gifs/gifno3.gif`,
       title: "Animated Illustration",
     },
-    {
-      src: `${import.meta.env.BASE_URL}img/gifs/collagio.jpg`,
-      title: "Animated Illustration",
-    },
+   
   ];
